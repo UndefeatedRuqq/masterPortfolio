cb27cdc3c517a7a2b3cf9cd7fbf03ce350d1a2f1
ruqqstandhere <141534361+ruqqstandhere@users.noreply.github.com>
2023-09-10 00:35:52 +0700

    nooode
    sdadsas

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,9 +19,9 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "ruqqnofearss",
+  logo_name: "UndefeatedRuqq",
+  nickname: "Faruq Adrean",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
